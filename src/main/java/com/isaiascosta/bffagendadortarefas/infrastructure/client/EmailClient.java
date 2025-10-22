1074b38ac2d242aba2376fad3220b6ef0ff28e26
Isaias Costa <isaiascssilva@gmail.com>
2025-10-22 15:25:07 -0300

    Adiconado Cron

--- src/main/java/com/isaiascosta/bffagendadortarefas/infrastructure/client/EmailClient.java
+++ src/main/java/com/isaiascosta/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -3,12 +3,14 @@ package com.isaiascosta.bffagendadortarefas.infrastructure.client;
 
 import com.isaiascosta.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 // Define um cliente Feign para comunicar com o microsserviço de usuário
 public interface EmailClient {
 
+  @PostMapping
   void enviarEmail(@RequestBody TarefasDTOResponse dto);
 }
 
